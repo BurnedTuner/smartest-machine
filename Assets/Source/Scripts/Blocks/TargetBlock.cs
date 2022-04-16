62d1538023ed36ecfd0502ea3295d0c422e68832
BurnedTuner <ainol0912@gmail.com>
2022-04-16 19:35:52 +0300

    Added Block Sprites

--- Assets/Source/Scripts/Blocks/TargetBlock.cs
+++ Assets/Source/Scripts/Blocks/TargetBlock.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 public class TargetBlock : Block
 {
     [SerializeField] private SpriteRenderer _spriteRenderer;
-    [SerializeField] private Color _hitColor;
+    [SerializeField] private Sprite _hitSprite;
     [SerializeField] private int _requiredPower;
 
     public event Action<TargetBlock> Activated;
 
     protected override void OnBulletCollision(Bullet bullet, Vector3 normal)
     {
-        _spriteRenderer.color = _hitColor;
+        _spriteRenderer.sprite = _hitSprite;
         Activated?.Invoke(this);
 
         bullet.Hit();
--- Assets/Source/Scripts/Bullet.cs
+++ Assets/Source/Scripts/Bullet.cs
@@ -3,26 +3,18 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class Bullet : MonoBehaviour
 {
-    [SerializeField] private float _startSpeed;
-    [SerializeField] private float _maxSpeed;
-    [SerializeField] private float _acceleraion;
+    [SerializeField] private float _speed;
 
-    private float _currentSpeed;
     private Rigidbody2D _rigidbody;
 
     private void Awake()
     {
-        _currentSpeed = _startSpeed;
         _rigidbody = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
-        _currentSpeed += _acceleraion;
-
-        _currentSpeed = Mathf.Clamp(_currentSpeed, 0, _maxSpeed);
-
-        _rigidbody.velocity = (Vector2)transform.right * _currentSpeed * Time.deltaTime;
+        _rigidbody.velocity = (Vector2)transform.right * _speed * Time.deltaTime;
     }
     public void SetAngle(float zAngle)
     {
